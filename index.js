b0913e18277b893d5f3db10a488d0a119330e5f9
JoaoCarvalho-dv <joaocarvalho122020@gmail.com>
2024-10-16 22:26:35 -0300

    Atualização

---
+++ index.js
@@ -0,0 +1,19 @@
+var controle = false;
+
+function iconOver(){
+    var icone = document.getElementById("iconex").style.visibility = hidden;
+}
+
+function iconOut(){
+    var icone = document.getElementById("iconey") = document.getElementById("iconex");
+}
+
+var controle = false;
+
+function iconOver(){
+    var icone = document.getElementById("iconew").style.visibility = hidden;
+}
+
+function iconOut(){
+    var icone = document.getElementById("iconez") = document.getElementById("iconex");
+}
